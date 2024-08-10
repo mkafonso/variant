27c74c0818ddb5edc63578f47117b91245213d7c
mkafonso <makaya2afonso@gmail.com>
2024-08-10 19:27:33 -0300

    fix: init sessions module

--- applications/backend/src/app.module.ts
+++ applications/backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common'
 import { AccountsModule } from './accounts/accounts.module'
 import { ExperimentsModule } from './experiments/experiments.module'
 import { ProductsModule } from './products/products.module'
+import { SessionsModule } from './sessions/sessions.module'
 
 @Module({
-  imports: [AccountsModule, ProductsModule, ExperimentsModule],
+  imports: [AccountsModule, ProductsModule, ExperimentsModule, SessionsModule],
   controllers: [],
   providers: [],
 })
